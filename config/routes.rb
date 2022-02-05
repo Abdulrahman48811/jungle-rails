1c833af498b71730d89a82b49ff795bc252db89f
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-02-05 08:02:20 +0000

    Feature: User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,9 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
-
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
